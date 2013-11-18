2080d4369b8d7751b764548019f44230f1c9abdf
jcollard <jcollard@unm.edu>
2013-11-18 17:43:16 -0500

    Added compose and path.

--- src/Graphics/Path.elm
+++ src/Graphics/Path.elm
@@ -1,5 +1,11 @@
-module Graphics.Path(Path, Location, loc, line) where
+module Graphics.Path(Path, 
+                     Location, 
+                     loc, 
+                     line,
+                     path,
+                     compose) where
 
+import Util
 import Graphics.Location as Location
 
 type Location = Location.Location
@@ -12,6 +18,15 @@ type Path
    , locationAt : Float -> Location
    }
 
+{-|
+
+  Given a start Location and an end Location produces
+  a Path that is a line between the Locations. The locationAt
+  0 is the start location and the locationAt (path.length) is the
+  end location. All inputs will fall upon the line between the
+  two points.
+
+ -}
 line : Location -> Location -> Path
 line start end =
     let dLeft = end.left - start.left
@@ -23,3 +38,46 @@ line start end =
               newTop = start.left + dTop*percentage
           in Location.loc (newLeft, newTop)
     in { length = length, locationAt = locationAt }
+       
+{-|       
+
+  Creates a path connecting each Location together in order from left to right.
+
+ -}
+path : [Location] -> Path
+path (a::b::ls) = Util.foldl compose (line a b) <| pathHelper a (b::ls)
+    
+pathHelper : Location -> [Location] -> [Path]
+pathHelper a (b::ls) = 
+  case ls of
+    [] -> [line a b]
+    _ -> line a b :: pathHelper b ls
+       
+{-|       
+
+  Compose two paths together into one with a line segment
+  connecting the end of the first path with the start of
+  the second path. The length of the path is the sum of the
+  two paths plus the line segment connectin them.
+
+ -}
+compose : Path -> Path -> Path
+compose first second =
+  let endFirst = first.locationAt first.length
+      startSecond = second.locationAt 0
+      mergePath = line endFirst startSecond
+      length = first.length + second.length + mergePath.length
+      firstWeight = first.length/length
+      mergePathWeight = mergePath.length/length
+      secondWeight = second.length/length
+      locationAt pos =
+        let percentage = pos/length
+            inFirst = percentage < firstWeight
+        in if inFirst then first.locationAt (percentage/firstWeight)
+           else 
+             let inMerge = (percentage - firstWeight) < mergePathWeight
+             in if inMerge then mergePath.locationAt ((percentage - firstWeight)/mergePathWeight)
+                else
+                  second.locationAt ((percentage - firstWeight - mergePathWeight)/secondWeight)
+  in {length = length, locationAt = locationAt }
+            
--- test/Graphics/PathTest.elm
+++ test/Graphics/PathTest.elm
@@ -5,7 +5,7 @@ import Test as T
 
 test = T.test "Graphics.Path" tests
 
-tests = testLineLength ++ testLocationAt
+tests = testLineLength ++ testLocationAt ++ testCompose
 
 epsilon = 0.0001
 
@@ -20,14 +20,22 @@ loc3 = loc (-1, -1)
 loc4 = loc (-1, -2)
 
 line0 = line loc0 loc1
+line1 = line loc1 loc2
 
-testLineLength = [testLineLength0]
+path0 = compose line0 line1
+
+testLineLength = [testLineLength0, testLineLength1]
 
 testLineLength0 = 
   testcase "testLineLength0" <|
   assertWithTolerance (sqrt 2) line0.length
   
-testLocationAt = [testLocationAt0, testLocationAt1, testLocationAt2, testLocationAt3]
+testLineLength1 =  
+  testcase "testLineLength1" <|
+  assertEquals 1 line1.length
+  
+testLocationAt = [testLocationAt0, testLocationAt1, testLocationAt2, testLocationAt3,
+                  testLocationAt4, testLocationAt5, testLocationAt6]
 
 testLocationAt0 =
   testcase "testLocationAt0" <|
@@ -44,3 +52,38 @@ testLocationAt2 =
 testLocationAt3 =
   testcase "testLocationAt3" <|
   assertWithTolerance (-1) (line0.locationAt -line0.length).top
+  
+testLocationAt4 =
+  testcase "testLocationAt4" <|
+  assertWithTolerance 1 (line1.locationAt 0).left
+
+testLocationAt5 =
+  testcase "testLocationAt5" <|
+  assertWithTolerance 1.5 (line1.locationAt 0.5).top
+
+testLocationAt6 =
+  testcase "testLocationAt6" <|
+  assertWithTolerance 1 (line1.locationAt 0.5).left
+  
+testCompose = [testCompose0, testCompose1, testCompose2,
+               testCompose3, testCompose4]
+
+testCompose0 =
+  testcase "testCompose0" <|
+  assertEquals (1 + (sqrt 2)) (compose line0 line1).length
+  
+testCompose1 =
+  testcase "testCompose1" <|
+  assertEquals (line0.locationAt 0) (path0.locationAt 0)
+  
+testCompose2 =
+  testcase "testCompose2" <|
+  assertEquals (line0.locationAt (sqrt 2)) (path0.locationAt (sqrt 2))
+  
+testCompose3 =
+  testcase "testCompose3" <|
+  assertEquals (line1.locationAt 0) (path0.locationAt (sqrt 2))
+  
+testCompose4 =
+  testcase "testCompose4" <|
+  assertWithTolerance 2 (path0.locationAt (1 + sqrt 2)).top
